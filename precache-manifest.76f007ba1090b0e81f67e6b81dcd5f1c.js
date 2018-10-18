232cf7fed0b9ceae928dfa6c0a82e77282652dc9
keren0908 <akayayy@gmail.com>
2018-10-18 16:32:38 -0400

    Updates

---
+++ precache-manifest.76f007ba1090b0e81f67e6b81dcd5f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "3d1d37796510cca6f562",
+    "url": "/contactmanager/static/js/main.3d1d3779.chunk.js"
+  },
+  {
+    "revision": "dfde8a98bba409708ac9",
+    "url": "/contactmanager/static/js/1.dfde8a98.chunk.js"
+  },
+  {
+    "revision": "dfde8a98bba409708ac9",
+    "url": "/contactmanager/static/css/1.051dc855.chunk.css"
+  },
+  {
+    "revision": "063db263cb70d456449e15e9bbcf3e44",
+    "url": "/contactmanager/index.html"
+  }
+];
