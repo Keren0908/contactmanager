a18aec14bc59ffa27e117ed686899b3e420e5cac
keren0908 <akayayy@gmail.com>
2018-10-18 15:00:12 -0400

    add 404 page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="display-4"><span className="text-danger">404</span> Pages Not Found</h1>
+      <p className="lead">Sorry, that page can not be reached.</p>
+    </div>
+  )
+}
