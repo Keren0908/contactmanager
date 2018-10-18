93d4a05cbf2fd52697a7b806b9ca1925d4129418
keren0908 <akayayy@gmail.com>
2018-10-18 14:57:46 -0400

    add redirect functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
 import Header from "./components/layout/Header";
+import About from "./components/pages/About";
 import { Provider } from "./context";
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -10,13 +13,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
+        <Router>
         <div className="App">
           <Header branding="Contact Manger" />
           <div className="container">
-            <AddContact />
-            <Contacts />
+            <Switch>
+              <Route exact path="/" component={Contacts} />
+              <Route exact path="/contact/add" component={AddContact} />
+              <Route exact path="/about" component={About} />
+            </Switch>
           </div>
         </div>
+        </Router>
       </Provider>
     );
   }
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div className="display-4">About Contact Manager</div>
+      <p className="lead">Simple app to manage contacts</p>
+      <div className="text-secondary">Version 1.0.0</div>
+    </div>
+  )
+}
+
+export default About;
